c5c59ddeb8f858937b1d160240ce231c86a8b991
ortiztud <fjavierot@gmail.com>
2021-04-12 15:38:48 +0200

    relocate init and upload table_session-1_hands-on

---
+++ init.m
@@ -0,0 +1,20 @@
+% This script imports some functions that we will need for the seminar. In 
+% Matlab's terms: it will add the relevant folders to Matlab's path.
+%
+% REMEMBER: You need to run this script every time that you open Matlab.
+% You can do that by clicking the green "play" button at the top of this
+% window or just typing "init" in Matlab's terminal.
+
+%% Local paths
+% Here you need to specify where you have put the SPM folder.
+spm_folder = '/home/ortiz/DATA/2_Analysis_Folder/_common_software/spm12';
+
+%% Add paths (no need to change anything from here on)
+% Seminar functions
+addpath('_functions');
+
+% The next line will make SPM's functions available to MATLAB.
+addpath(spm_folder)
+
+%% Print message
+sprintf('All necessary paths have been added. We are ready to start.')

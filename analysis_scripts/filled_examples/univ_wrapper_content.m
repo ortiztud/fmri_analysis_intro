191469fe6fec3210f0f761b3cdf337eb1af5c89c
ortiztud <javierortiz@air-de-javier.speedport_w_724v_typ_a_05011603_06_003>
2021-04-20 11:34:11 +0200

    upload filled examples

---
+++ analysis_scripts/filled_examples/univ_wrapper_content.m
@@ -0,0 +1,37 @@
+% Filled in example for fmri_analysis_intro. Content-specific dataset
+
+%% Define all the paths in your computer
+project_folder='/home/ortiz/DATA/2_Analysis_Folder/fMRI_analysis_seminar/content-specific';
+
+%% Which subjects?
+subjects = [6,7,8];
+
+%% Now we need to create the condition files. For that we can use the function called
+% univariate_00_create_cond_files(project_folder, subject, task)
+% Create condition files
+for c_sub = subjects
+    univariate_00_create_cond_files(project_folder, c_sub, 'floc', 'floc7T',{'adult';'house'})
+end
+
+% Create confound files
+for c_sub = subjects
+    univariate_01_create_confounds_files(project_folder, c_sub, 'floc', 'floc7T')
+end
+
+% Run GLM
+for c_sub=subjects
+    univariate_02_glm(project_folder, c_sub, 'floc', 1, 'floc7T')
+end
+
+% Run contrast
+contrast_names={'adult-house';'house-adult'};
+contrast_weights={[1 -1];[-1 1]};
+for c_sub=subjects
+    univariate_03_contrast(project_folder, c_sub, contrast_names, contrast_weights) % Inc > Neutr
+end
+
+% Run group analysis for each contrast
+for c_cont = 1:numel(contrast_names)
+    univariate_04_group_level(project_folder, subjects, c_cont, ...
+        contrast_names{c_cont});
+end
---
+++ analysis_scripts/filled_examples/univ_wrapper_process.m
@@ -0,0 +1,37 @@
+% Filled in example for fmri_analysis_intro. Process-specific dataset
+
+%% Define all the paths in your computer
+project_folder='/home/ortiz/DATA/2_Analysis_Folder/fMRI_analysis_seminar/process-specific';
+
+%% Which subjects?
+subjects = [1:10];
+
+%% Now we need to create the condition files. For that we can use the function called
+% univariate_00_create_cond_files(project_folder, subject, task)
+% Create condition files
+for c_sub = subjects
+    univariate_00_create_cond_files(project_folder, c_sub, 'stroop')
+end
+
+% Create confound files
+for c_sub = subjects
+    univariate_01_create_confounds_files(project_folder, c_sub, 'stroop')
+end
+
+% Run GLM
+for c_sub=subjects
+    univariate_01_glm(project_folder, c_sub, 'stroop', 1)
+end
+
+% Run contrast
+contrast_names={'inc-neu';'neu-inc';'inc-all';'all-inc';'con-inc';'con-neu'};
+contrast_weights={[0 1 -1];[0 -1 1];[-1 2 -1];[1 -2 1];[1 -1 0];[1 0 -1]};
+for c_sub=subjects
+    univariate_02_contrast(project_folder, c_sub, contrast_names, contrast_weights) % Inc > Neutr
+end
+
+% Run group analysis for each contrast
+for c_cont = 1:numel(contrast_names)
+    univariate_03_group_leveI(project_folder, [1:28], c_cont, ...
+        contrast_names{c_cont});
+end

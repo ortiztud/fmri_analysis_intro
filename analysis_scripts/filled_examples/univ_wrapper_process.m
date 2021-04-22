a62ff7c0bf527a12bc004d2135562e5c27f714b6
ortiztud <ortiztudela@psych.uni-frankfurt.de>
2021-04-23 01:14:18 +0200

    update examples!

--- analysis_scripts/filled_examples/univ_wrapper_process.m
+++ analysis_scripts/filled_examples/univ_wrapper_process.m
@@ -1,10 +1,10 @@
 % Filled in example for fmri_analysis_intro. Process-specific dataset
 
 %% Define all the paths in your computer
-project_folder='/home/ortiz/DATA/2_Analysis_Folder/fMRI_analysis_seminar/process-specific';
+project_folder='/Users/javierortiz/PowerFolders/data_fmri-analysis_intro/process-specific';
 
 %% Which subjects?
-subjects = [1:10];
+subjects = [4,5];
 
 %% Now we need to create the condition files. For that we can use the function called
 % univariate_00_create_cond_files(project_folder, subject, task)
@@ -20,18 +20,18 @@ end
 
 % Run GLM
 for c_sub=subjects
-    univariate_01_glm(project_folder, c_sub, 'stroop', 1)
+    univariate_02_glm(project_folder, c_sub, 'stroop', 1)
 end
 
 % Run contrast
 contrast_names={'inc-neu';'neu-inc';'inc-all';'all-inc';'con-inc';'con-neu'};
 contrast_weights={[0 1 -1];[0 -1 1];[-1 2 -1];[1 -2 1];[1 -1 0];[1 0 -1]};
 for c_sub=subjects
-    univariate_02_contrast(project_folder, c_sub, contrast_names, contrast_weights) % Inc > Neutr
+    univariate_03_contrast(project_folder, c_sub, contrast_names, contrast_weights) % Inc > Neutr
 end
 
 % Run group analysis for each contrast
 for c_cont = 1:numel(contrast_names)
-    univariate_03_group_leveI(project_folder, [1:28], c_cont, ...
+    univariate_04_group_level(project_folder, subjects, c_cont, ...
         contrast_names{c_cont});
 end
